ecb7f4e52f0dbc25a6a44ae623cf59440aeb3dd7
uupt <270628297@qq.com>
2025-06-30 12:15:47 +0800

    Update: 预定义限定符。写法二

--- hilt/src/main/java/com/example/hilt/di/module/AppModule.kt
+++ hilt/src/main/java/com/example/hilt/di/module/AppModule.kt
@@ -2,15 +2,16 @@ package com.example.hilt.di.module
 
 import android.app.Activity
 import android.app.Application
+import android.content.Context
 import com.example.hilt.bean.User
 import com.example.hilt.viewmodel.MainViewModel
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import androidx.activity.ComponentActivity
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.qualifiers.ActivityContext
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ActivityScoped
-import javax.inject.Singleton
 
 @Module
 @InstallIn(ActivityComponent::class)
@@ -22,13 +23,16 @@ class AppModule {
         return User("provideUser")
     }
 
+    // 需要去掉MainViewModel上的@Inject、@ActivityContext注解
     @Provides
     fun provideMainViewModel(
         user: User,
         application: Application,
         // SingletonComponent 不能提供 Activity
-        activity: Activity
+        activity: Activity,
+        // @ApplicationContext context: Context
+        @ActivityContext context: Context
     ): MainViewModel {
-        return MainViewModel(user, application, activity)
+        return MainViewModel(user, application, activity, context)
     }
 }
--- hilt/src/main/java/com/example/hilt/viewmodel/MainViewModel.kt
+++ hilt/src/main/java/com/example/hilt/viewmodel/MainViewModel.kt
@@ -5,17 +5,14 @@ import android.app.Application
 import android.content.Context
 import android.util.Log
 import com.example.hilt.bean.User
-import dagger.hilt.android.qualifiers.ActivityContext
-import javax.inject.Inject
-
 private const val TAG = "MainViewModel"
 
 // 已经绑定的类
- class MainViewModel @Inject constructor(
+class MainViewModel (
     private val user: User,
     private val application: Application,
     private val activity: Activity,
-    @ActivityContext private val context: Context
+    private val context: Context
 ) {
     fun introduction() {
         Log.i(TAG, "introduction: user = $user, application = $application")
